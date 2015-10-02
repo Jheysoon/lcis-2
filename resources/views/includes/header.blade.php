113cd348e59aaa41a6b9d038620b78a1df54a27c
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-02 18:25:51 +0800

    Initial commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+    <title>{{ $title }}</title>
+    <link rel="stylesheet" href="<?php echo asset('assets/css/bootstrap.css') ?>">
+    <link rel="stylesheet" href="<?php echo asset('assets/css/material.css') ?>">
+    <link rel="stylesheet" href="<?php echo asset('assets/css/style.css') ?>">
+</head>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+@include('includes/header', ['title' => 'Login'])
+<body>
+    <!-- =================================== login ===================================== -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 header">
+            <div class="col-md-8">
+
+            </div>
+            <div class = "col-md-4">
+                <form class="form-horizontal login" action="/" method="post">
+                    <h2 class="sign">
+                        <b>Sign in</b>
+                    </h2>
+                    <br/>
+                    <div class="form-group">
+                        <div class="col-sm-12">
+                            <input type="text" class="form-control floating-label" name="username" id="username" autofocus placeholder="Username" value="" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-12">
+                            <input type="password" class="form-control floating-label" name="password" id="password" placeholder="Password" value="" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-9">
+                            <input type="submit" class="btn btn-success pull-right btn-raised" name="name" value="Sign in">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <br>
+    </div>
+</div>
+@include('includes/footer')
+</body>
+</html>
