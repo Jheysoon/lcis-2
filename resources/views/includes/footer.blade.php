226290f9cf4c1072b509d98ce2cbd0ef2ae7aad7
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-04 02:34:00 +0800

    remove ripple.js

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,6 @@
 <script type="text/javascript" src="<?php echo asset('assets/js/jquery.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo asset('assets/js/bootstrap.js') ?>"></script>
 <script type="text/javascript" src="<?php echo asset('assets/js/material.js') ?>"></script>
-<script type="text/javascript" src="<?php echo asset('assets/js/ripples.js') ?>"></script>
 <script type="text/javascript">
     $.material.init();
 </script>
