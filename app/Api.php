de54d2892233665bcfb24f721f0a83e8c0594df0
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-04 10:05:37 +0800

    add function to check conflict

--- app/Api.php
+++ app/Api.php
@@ -129,7 +129,29 @@ class Api
             DB::table('out_exception')->insert($b);
 			return 'Curriculum not fount';
         }
+    }
 
+    // 1:00-3:00 / 2:00-5:00
 
+	//$from = 1:00,	$from_compare 	= 2:00
+	//$to 	= 3:00,	$to_compare 	= 5:00
+    function intersectCheck($from, $from_compare, $to, $to_compare)
+	{
+        $from 			= strtotime($from);
+        $from_compare 	= strtotime($from_compare);
+        $to 			= strtotime($to);
+        $to_compare 	= strtotime($to_compare);
+        $intersect 		= min($to, $to_compare) - max($from, $from_compare);
+
+            if ( $intersect < 0 ) $intersect = 0;
+            $overlap = $intersect / 3600;
+            if ( $overlap <= 0 ):
+                // There are no time conflicts
+                return FALSE;
+                else:
+                // There is a time conflict
+                // echo '<p>There is a time conflict where the times overlap by ' , $overlap , ' hours.</p>';
+                return TRUE;
+            endif;
     }
 }
