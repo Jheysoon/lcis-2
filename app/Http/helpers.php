456c5d978689f058c61cf099db0c98ef0b8ff12a
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-06 08:57:05 +0800

    add helper file

---
+++ app/Http/helpers.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Http\Request;
+
+if(! function_exists('set_value'))
+{
+    // for testing
+    function set_value($field, $default = '')
+	{
+        return \Request::get($field, $default);
+	}
+}
+
+// for testing
+if ( ! function_exists('set_select'))
+{
+	function set_select($field, $value = '', $default = FALSE)
+	{
+        $input = \Request::get($field);
+		if ($input === NULL)
+		{
+			return ($default === TRUE) ? ' selected="selected"' : '';
+		}
+
+		$value = (string) $value;
+		if (is_array($input))
+		{
+			// Note: in_array('', array(0)) returns TRUE, do not use it
+			foreach ($input as &$v)
+			{
+				if ($value === $v)
+				{
+					return ' selected="selected"';
+				}
+			}
+
+			return '';
+		}
+
+		return ($input === $value) ? ' selected="selected"' : '';
+	}
+}
+
+// for testing
+if ( ! function_exists('set_checkbox'))
+{
+	function set_select($field, $value = '', $default = FALSE)
+	{
+        $input = \Request::get($field);
+		if ($input === NULL)
+		{
+			return ($default === TRUE) ? ' checked="checked"' : '';
+		}
+
+		$value = (string) $value;
+		if (is_array($input))
+		{
+			// Note: in_array('', array(0)) returns TRUE, do not use it
+			foreach ($input as &$v)
+			{
+				if ($value === $v)
+				{
+					return ' checked="checked"';
+				}
+			}
+
+			return '';
+		}
+
+		return ($input === $value) ? ' checked="checked"' : '';
+	}
+}
