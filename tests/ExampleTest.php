7c859a3f9c4c9ebb611ec2b99cdba91f8651f853
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-09 13:15:49 +0800

    comment

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -11,9 +11,9 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicExample()
-    {
-        $this->visit('/')
-             ->see('Laravel 5');
-    }
+    // public function testBasicExample()
+    // {
+    //     $this->visit('/')
+    //          ->see('Laravel 5');
+    // }
 }
