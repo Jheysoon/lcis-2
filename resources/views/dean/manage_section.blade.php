7a9c08469000d84f7cfc2a441c81061df51641e6
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-15 17:51:14 +0800

    fix markup

--- resources/views/dean/manage_section.blade.php
+++ resources/views/dean/manage_section.blade.php
@@ -7,83 +7,83 @@
 @section('body')
 	<div class="col-md-3"></div>
 	<div class="col-md-9">
-		<div class="panel p-body">
+		<div class="panel p-body panel-success">
 			<div class="panel-heading search">
 				<div class="col-md-12">
 					<h4>Manage Section</h4>
 				</div>
 			</div>
-		</div>
-		<div class="panel-body">
-			<div class="form-group">
-				<div class="col-sm-12">
-					@include('edp.cl_status')
+			<div class="panel-body">
+				<div class="form-group">
+					<div class="col-sm-12">
+						@include('edp.cl_status')
 
-					@if ($system->classallocationstatus == 1)
+						@if ($system->classallocationstatus == 1)
 
-						@if ($count < 1)
-							<a href="/non_exist" class="btn btn-primary btn pull-right">Add Non - Existing Subject</a>
-							<span class="clearfix"></span>
-							<br/>
+							@if ($count < 1)
+								<a href="/non_exist" class="btn btn-primary btn pull-right">Add Non - Existing Subject</a>
+								<span class="clearfix"></span>
+								<br/>
 
-							<table class="table table-bordered">
-								<caption>
-									Preparation for Academicterm SY:
-									<?php $sy = App\Academicterm::find($system) ?>
-									{{ $sy->systart.' - '.$sy->syend.' Term: '.$sy->term }}
+								<table class="table table-bordered">
+									<caption>
+										Preparation for Academicterm SY:
+										<?php $sy = App\Academicterm::find($system) ?>
+										{{ $sy->systart.' - '.$sy->syend.' Term: '.$sy->term }}
 
-									@if (Session::get('uid') == $system->employeeid)
-										<?php $col = DB::table('tbl_college')->where('id', $owner)->first() ?>
-										{{ 'College'.$col->description }}
-									@endif
+										@if (Session::get('uid') == $system->employeeid)
+											<?php $col = DB::table('tbl_college')->where('id', $owner)->first() ?>
+											{{ 'College'.$col->description }}
+										@endif
 
-								</caption>
-								<tr>
-									<th>Subject</th>
-									<th>Description</th>
-									<th>Course</th>
-									<th>Year Level</th>
-									<th>No. of Student</th>
-									<th>Apprx. Section <br/>({{ $system->numberofstudent }} students)</th>
-									<th>Section</th>
-									<th>Action</th>
-								</tr>
-
-								@foreach ($sql as $subj)
+									</caption>
 									<tr>
-										<td> {{ $subj->code }} </td>
-										<td> {{ $subj->descriptivetitle }} </td>
-										<td> 
-											<?php $course = DB::table('tbl_course')->where('id', $subj->coursemajor)->first(); ?>
-											{{ $course->shortname }}
-										</td>
-										<td> {{ $subj->yearlevel }} </td>
-										<td> {{ round($subj->section) }} </td>
-										<form class="addClassAllocation">
-											<td>
-												<input type="number" min="1" data-param="{{ round($subj->section) }}" class="form-control input-sm section" name="sections" value="{{ $subj->section }}" required>
-												<input type="hidden" name="out_section_id" value="{{ $subj->id }}">
-											</td>
-											<td>
-												<input type="submit" value="Update" class="btn btn-primary btn-sm">
-											</td>
-										</form>
+										<th>Subject</th>
+										<th>Description</th>
+										<th>Course</th>
+										<th>Year Level</th>
+										<th>No. of Student</th>
+										<th>Apprx. Section <br/>({{ $system->numberofstudent }} students)</th>
+										<th>Section</th>
+										<th>Action</th>
 									</tr>
-								@endforeach
 
-							</table>
-							<a href="/dean/add_task_comp/2/O" class="btn btn-primary pull-right">Attest all</a>
+									@foreach ($sql as $subj)
+										<tr>
+											<td> {{ $subj->code }} </td>
+											<td> {{ $subj->descriptivetitle }} </td>
+											<td> 
+												<?php $course = DB::table('tbl_course')->where('id', $subj->coursemajor)->first(); ?>
+												{{ $course->shortname }}
+											</td>
+											<td> {{ $subj->yearlevel }} </td>
+											<td> {{ round($subj->section) }} </td>
+											<form class="addClassAllocation">
+												<td>
+													<input type="number" min="1" data-param="{{ round($subj->section) }}" class="form-control input-sm section" name="sections" value="{{ $subj->section }}" required>
+													<input type="hidden" name="out_section_id" value="{{ $subj->id }}">
+												</td>
+												<td>
+													<input type="submit" value="Update" class="btn btn-primary btn-sm">
+												</td>
+											</form>
+										</tr>
+									@endforeach
+
+								</table>
+								<a href="/dean/add_task_comp/2/O" class="btn btn-primary pull-right">Attest all</a>
+							@else
+								<div class="alert alert-danger center-block" style="max-width:400px;text-align:center;">
+									You have attested this
+								</div>
+							@endif
+
 						@else
-							<div class="alert alert-danger center-block" style="max-width:400px;text-align:center;">
-								You have attested this
+							<div class="alert alert-danger center-block" style="max-width:400px;">
+								Cannot run this program. The EDP must complete the step 1.
 							</div>
 						@endif
-
-					@else
-						<div class="alert alert-danger center-block" style="max-width:400px;">
-							Cannot run this program. The EDP must complete the step 1.
-						</div>
-					@endif
+					</div>
 				</div>
 			</div>
 		</div>
