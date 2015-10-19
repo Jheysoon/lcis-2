0e661e2c3775aaf99118d86b08ea3fee1403003d
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-19 15:35:18 +0800

    simplify init classallocation view

--- app/Http/Controllers/Edp/ClassAlloc.php
+++ app/Http/Controllers/Edp/ClassAlloc.php
@@ -21,11 +21,50 @@ class ClassAlloc extends Controller
 
     function init()
     {
-    	$data['system'] = $this->system;
-    	$data['c']		= DB::table('tbl_completion')->where('stage', 2)
+    	$data['val'] = '';
+    	if ($this->system->phaseterm == env('FIN')) {
+
+    		if ($this->system->classallocationstatus == 2 OR
+    			$this->system->classallocationstatus == 1) {
+    			$c		= DB::table('tbl_completion')->where('stage', 2)
 							->where('academicterm', $system->phaseterm)
 							->where('status', 'O');
 
+    			if ($c == env('COLLEGE_COUNT')) {
+    				// update the systemvalues
+					DB::table('tbl_systemvalues')->update(['classallocationstatus' => 3]);
+					// delete classallocation for this phaseterm
+					DB::table('tbl_classallocation')->where('academicterm', $system->phaseterm)
+						->delete();
+					$sec = DB::table('out_section')->get();
+
+					foreach ($sec as $section) {
+
+						// if the section is zero it will not satisfy this condition
+						for ($i = 1; $i <= $section->section; $i++) {
+							$data['academicterm'] 	= $ssection->academicterm;
+							$data['coursemajor'] 	= $ssection->coursemajor;
+							$data['subject'] 		= $ssection->subject;
+							$data['instructor']		= 0;
+							$data['reserved']		= 0;
+							$data['enrolled']		= 0;
+							DB::table('tbl_classallocation')->insert($data);
+						}
+					}
+
+					$data['val'] = 'OK';
+    			} else {
+    				$data['val'] = 'college count';
+    			}
+
+    		} else {
+    			$data['val'] = 'cannot run';
+    		}
+
+    	} else {
+    		$data['val'] = 'cannot run in this phase';
+    	}
+    	
     	return view('edp.initClass', $data);
     }
 }
