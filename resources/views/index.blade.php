22f892041841f69223636a28a523ecbe9fc71d27
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-22 17:12:27 +0800

    user helper function

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@
                         </h2>
                         <br/>
                         <?php echo $error ?>
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        {!! csrf_field() !!}
                         <div class="form-group">
                             <div class="col-sm-12">
                                 <input type="text" class="form-control floating-label" name="username" id="username" autofocus placeholder="Username" value="{{ request('username') }}" required>
