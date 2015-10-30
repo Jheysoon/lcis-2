8f5a080557bcdfce4ba3554fcf40a8968d951300
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-31 00:29:55 +0800

    redesign

--- resources/views/edp/dean_activity.blade.php
+++ resources/views/edp/dean_activity.blade.php
@@ -2,25 +2,53 @@
     <div class="alert alert-danger" style="text-align:center;">
         {{ $message }}
     </div>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <div class="panel-title" style="text-align:center;">
-                Dean's Activity
-            </div>
+
+    <div class="mdl-card mdl-shadow--4dp">
+        <div class="mdl-card__title mdl-card--expand text-center">
+            <h1 class="mdl-card__title-text text-center">Dean's Activity</h1>
         </div>
-
-        <table class="table table-bordered">
-            <tr>
-                <td style="text-align:center;">College</td>
-                <td style="text-align:center;">Status</td>
-                <td style="text-align:center;">Date Completed</td>
-            </tr>
-
-            @foreach ($colleges as $college)
+        <div class="mdl-card__supporting-text" style="width:100%;">
+            <table class="table table-bordered">
                 <tr>
-                    <td>{{ $college->description }}</td>
+                    <td style="text-align:center;">College</td>
+                    <td style="text-align:center;">Status</td>
+                    <td style="text-align:center;">Date Completed</td>
+                </tr>
+
+                @foreach ($colleges as $college)
+                    <tr>
+                        <td>{{ $college->description }}</td>
+                        <td style="text-align:center;">
+                            <?php $c = DB::table('tbl_completion')->where('completedby', $college->dean)->where('stage', $stage); ?>
+                            
+                            @if ($c->count() > 0)
+                                <?php $cc = $c->first() ?>
+
+                                @if ($cc->status == 'O')
+                                    Attested
+                                @endif
+
+                            @else
+                                Untouched
+                            @endif
+
+                        </td>
+                        <td style="text-align:center;">
+
+                            @if ($c->count() > 0)
+                                {{ $cc->statusdate }}
+                            @else
+                                Not Available
+                            @endif
+
+                        </td>
+                    </tr>
+                @endforeach
+
+                <tr>
+                    <td>COMPUTER SUBJECTS AND NSTP</td>
                     <td style="text-align:center;">
-                        <?php $c = DB::table('tbl_completion')->where('completedby', $college->dean)->where('stage', $stage); ?>
+                        <?php $c = DB::table('tbl_completion')->where('completedby', $system->employeeid)->where('stage', $stage); ?>
                         
                         @if ($c->count() > 0)
                             <?php $cc = $c->first() ?>
@@ -32,45 +60,18 @@
                         @else
                             Untouched
                         @endif
-
+                        
                     </td>
                     <td style="text-align:center;">
-
                         @if ($c->count() > 0)
                             {{ $cc->statusdate }}
                         @else
-                            Not Available
+                            Not Available'
                         @endif
-
                     </td>
                 </tr>
-            @endforeach
-
-            <tr>
-                <td>COMPUTER SUBJECTS AND NSTP</td>
-                <td style="text-align:center;">
-                    <?php $c = DB::table('tbl_completion')->where('completedby', $system->employeeid)->where('stage', $stage); ?>
-                    
-                    @if ($c->count() > 0)
-                        <?php $cc = $c->first() ?>
-
-                        @if ($cc->status == 'O')
-                            Attested
-                        @endif
-
-                    @else
-                        Untouched
-                    @endif
-                    
-                </td>
-                <td style="text-align:center;">
-                    @if ($c->count() > 0)
-                        {{ $cc->statusdate }}
-                    @else
-                        Not Available'
-                    @endif
-                </td>
-            </tr>
-        </table>
+            </table>
+        </div>
     </div>
+    <br/>
 </div>
