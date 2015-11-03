3f6f8023e82e61c66d58ebb6b3c0ce26196d45ff
Jayson Martinez <jlmartinez631@gmail.com>
2015-11-03 18:02:31 +0800

    remove unuse function

--- app/Classallocation.php
+++ app/Classallocation.php
@@ -12,30 +12,25 @@ class Classallocation extends Model
     protected $table    = 'tbl_classallocation';
     public $timestamps  = false;
 
-    public static function getStudEnrol($cid, $acam)
-    {
-        
-    }
-
     public static function getAlloc($owner, $system)
     {
     	if ($system->employeeid == Auth::user()->id) {
     		return DB::select("SELECT a.id as cid, coursemajor, descriptivetitle, code, instructor
 				FROM tbl_classallocation a, tbl_subject b
-				WHERE a.subject = b.id AND academicterm = $system->phaseterm 
+				WHERE a.subject = b.id AND academicterm = $system->phaseterm
 				AND (computersubject = 1 OR nstp = 1)
 				ORDER BY b.code ASC, coursemajor ASC, a.id ASC");
     	} elseif ($owner == 1) {
     		return DB::select("SELECT a.id as cid, coursemajor, descriptivetitle, code, instructor
 				FROM tbl_classallocation a, tbl_subject b
-				WHERE a.subject = b.id AND academicterm = $system->phaseterm 
+				WHERE a.subject = b.id AND academicterm = $system->phaseterm
 				AND (owner = 1 OR gesubject = 1) AND computersubject = 0 AND nstp = 0
 				ORDER BY b.code ASC, coursemajor ASC, a.id ASC");
     	} else {
     		return DB::select("SELECT a.id as cid, coursemajor, descriptivetitle, code, instructor
 				FROM tbl_classallocation a, tbl_subject b
-				WHERE a.subject = b.id AND academicterm = $system->phaseterm 
-				AND computersubject = 0 AND gesubject = 0 
+				WHERE a.subject = b.id AND academicterm = $system->phaseterm
+				AND computersubject = 0 AND gesubject = 0
 				AND owner = $owner AND nstp = 0
 				ORDER BY b.code ASC, coursemajor ASC, a.id ASC");
     	}
@@ -43,7 +38,7 @@ class Classallocation extends Model
 
     public function getSubject()
     {
-                            // model  foreign_key              
+                            // model  foreign_key
         return $this->belongsTo('App\Subject', 'subject');
     }
 
