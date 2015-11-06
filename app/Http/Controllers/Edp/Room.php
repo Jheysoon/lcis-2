c76632383eaa7d8ac7ae18c29b7e28f84eb97741
Jayson Martinez <jlmartinez631@gmail.com>
2015-11-06 11:17:15 +0800

    remove unused variable

--- app/Http/Controllers/Edp/Room.php
+++ app/Http/Controllers/Edp/Room.php
@@ -18,17 +18,6 @@ class Room extends Controller
 {
     public $system;
 
-    public $table_day;
-
-    public $color = array(
-                    '#BA68C8','#0097A7',
-                    '#F06292','#039BE5',
-                    '#E57373','#00C853',
-                    '#7986CB','#689F38',
-                    '#9575CD','#43A047',
-                    '#009688','#EF6C00'
-                );
-
     function __construct()
     {
         $this->middleware('auth');
