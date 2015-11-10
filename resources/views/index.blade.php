56b9f1ab0c0a0da10e9de31bfedc8e485c6ce3af
Jayson Martinez <jlmartinez631@gmail.com>
2015-11-10 15:31:08 +0800

    use bootstrap card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,11 +23,11 @@
                 <div class = "col-md-4">
                     <form class="form-horizontal" action="/" method="post">
                         <br/>
-                        <div class="mdl-card mdl-shadow--4dp" style="width: 330px;">
-                            <div class="mdl-card__title mdl-card--expand text-center">
-                                <h1 class="mdl-card__title-text text-center">Sign in</h1>
+                        <div class="card pull-right" style="width: 330px;">
+                            <div class="card-block">
+                                <h2 class="card-title">Sign In</h2>
                             </div>
-                            <div class="mdl-card__supporting-text">
+                            <div class="card-block">
                                 {!! $error !!}
                                 {!! csrf_field() !!}
                                 <div class="form-group">
@@ -40,8 +40,6 @@
                                         <input type="password" class="form-control floating-label" name="password" id="password" placeholder="Password" value="" required>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="mdl-card__actions mdl-card--border">
                                 <div class="col-sm-offset-3 col-sm-9">
                                     <input type="submit" class="btn btn-success pull-right btn-raised" name="name" value="Sign in">
                                 </div>
@@ -56,49 +54,55 @@
         <div class="row">
             <div class="col-sm-4">
                 <br/>
-                <div class="mdl-card mdl-shadow--4dp">
-                    <div class="mdl-card__title mdl-card--expand text-center mdl-card--border">
-                        <h1 class="mdl-card__title-text text-center">Goals</h1>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Goals</h3>
                     </div>
-                    <div class="mdl-card__supporting-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <br/>
-                <div class="mdl-card mdl-shadow--4dp">
-                    <div class="mdl-card__title mdl-card--expand text-center mdl-card--border">
-                        <h1 class="mdl-card__title-text text-center">Mission</h1>
-                    </div>
-                    <div class="mdl-card__supporting-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <div class="card-block">
+                        <p class="card-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <br/>
-                <div class="mdl-card mdl-shadow--4dp">
-                    <div class="mdl-card__title mdl-card--expand text-center mdl-card--border">
-                        <h1 class="mdl-card__title-text text-center">Vision</h1>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Mission</h3>
                     </div>
-                    <div class="mdl-card__supporting-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <div class="card-block">
+                        <p class="card-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <br/>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Vision</h3>
+                    </div>
+                    <div class="card-block">
+                        <p class="card-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
                     </div>
                 </div>
             </div>
