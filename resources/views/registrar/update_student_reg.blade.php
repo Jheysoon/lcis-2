b1997797baa2ed4da29ca19eae73c7225d8ab566
Jayson Martinez <jlmartinez631@gmail.com>
2015-11-14 22:06:27 +0800

    additional fields

--- resources/views/registrar/update_student_reg.blade.php
+++ resources/views/registrar/update_student_reg.blade.php
@@ -15,15 +15,17 @@
             </div>
             <div class="card-block">
                 <div class="col-md-4 col-md-offset-8">
-                    <form action="/search_registration" method="post">
+                    <form action="{{ url('search_student') }}" method="post">
                         {!! csrf_field() !!}
+                        <input type="hidden" name="redirect" value="update_registration">
                         <input type="text" style="width:250px;" class="form-control" name="student" placeholder="Search for students" value="">
                         <input type="submit" class="btn btn-primary btn-sm pull-right" name="name" value="Search">
                     </form>
                 </div>
             </div>
 
-            <form class="" action="index.html" method="post">
+            <form action="{{ url('update_student_reg') }}" method="post">
+                <input type="hidden" name="student" value="{{ $id or '' }}">
 
                 @include('registrar.registration_form')
 
