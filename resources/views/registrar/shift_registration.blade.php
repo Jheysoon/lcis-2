92a12d3196625bf72d67bb5f2bab9abe31cf2c79
Jayson Martinez <jlmartinez631@gmail.com>
2015-11-16 14:01:44 +0800

    change redirect value

--- resources/views/registrar/shift_registration.blade.php
+++ resources/views/registrar/shift_registration.blade.php
@@ -22,7 +22,7 @@
                 <div class="col-md-4 col-md-offset-8">
                     <form action="{{ url('search_student') }}" method="post">
                         {!! csrf_field() !!}
-                        <input type="hidden" name="redirect" value="update_registration">
+                        <input type="hidden" name="redirect" value="shift_student">
                         <input type="text" style="width:250px;" class="form-control" name="student" placeholder="Search for students" value="">
                         <input type="submit" class="btn btn-primary btn-sm pull-right" name="name" value="Search">
                     </form>
