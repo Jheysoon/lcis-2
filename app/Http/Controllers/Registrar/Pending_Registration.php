b62810b5c265226ce0404bf9ec42877318fb733d
Jayson Martinez <jlmartinez631@gmail.com>
2015-11-16 17:47:01 +0800

    authenticate

--- app/Http/Controllers/Registrar/Pending_Registration.php
+++ app/Http/Controllers/Registrar/Pending_Registration.php
@@ -11,6 +11,11 @@ use App\Http\Controllers\Controller;
 
 class Pending_Registration extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $data['registrations'] = Registration::pending()->paginate(15);
