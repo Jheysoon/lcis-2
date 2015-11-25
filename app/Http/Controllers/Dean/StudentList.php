5486c6bdf373e7ad99a09e5e3dfe9231b77e8585
Jayson Martinez <jlmartinez631@gmail.com>
2015-11-25 11:07:29 +0800

    change query

--- app/Http/Controllers/Dean/StudentList.php
+++ app/Http/Controllers/Dean/StudentList.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Dean;
 
 use DB;
 use App\Library\Api;
+use App\Academicterm;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -19,14 +20,36 @@ class StudentList extends Controller
         $data['isDisabled'] = ($system->phase != env('FIN') OR $system->classallocationstatus < 99) ? 'disabled' : '';
         $owner              = Api::getCollege();
         
-        $data['students'] = DB::table('tbl_registration')
-                            ->join('tbl_coursemajor', 'tbl_registration.coursemajor', '=', 'tbl_coursemajor.id')
-                            ->join('tbl_course', 'tbl_coursemajor.course', '=' ,'tbl_course.id')
-                            ->join('tbl_party', 'tbl_party.id', '=', 'tbl_registration.student')
-                            ->where('tbl_course.college', $owner)
-                            ->where('tbl_registration.status', '!=', 'G')
-                            ->groupBy('student')->orderBy('legacyid', 'DESC')
-                            ->select('firstname', 'lastname', 'description')->paginate(15);
+        $academicterm = Academicterm::find($system->currentacademicterm);
+        
+        if ($academicterm->term == 2) {
+            $acam               = $academicterm->id - 1;
+            $data['students']   = DB::table('tbl_enrolment')
+                                ->join('tbl_party', 'tbl_party.id', '=', 'tbl_enrolment.student')
+                                ->join('tbl_registration', 'tbl_registration.student', '=', 'tbl_enrolment.student')
+                                ->join('tbl_coursemajor', 'tbl_registration.coursemajor', '=', 'tbl_coursemajor.id')
+                                ->join('tbl_course', 'tbl_course.id', '=', 'tbl_coursemajor.course')
+                                ->where('tbl_course.college', $owner)
+                                ->where('tbl_enrolment.academicterm', $acam)
+                                ->orderBy('legacyid', 'DESC')
+                                ->groupBy('tbl_enrolment.student')
+                                ->select('firstname', 'lastname' ,'description')
+                                ->paginate(15);
+        
+        } elseif ($academicterm->term == 1) {
+            $acam               = $academicterm->id - 2;
+            $data['students']   = DB::table('tbl_enrolment')
+                                ->join('tbl_party', 'tbl_party.id', '=', 'tbl_enrolment.student')
+                                ->join('tbl_registration', 'tbl_registration.student', '=', 'tbl_enrolment.student')
+                                ->join('tbl_coursemajor', 'tbl_registration.coursemajor', '=', 'tbl_coursemajor.id')
+                                ->join('tbl_course', 'tbl_course.id', '=', 'tbl_coursemajor.course')
+                                ->where('tbl_course.college', $owner)
+                                ->where('tbl_enrolment.academicterm', $acam)
+                                ->orderBy('legacyid', 'DESC')
+                                ->groupBy('tbl_enrolment.student')
+                                ->select('firstname', 'lastname' ,'description')
+                                ->paginate(15);
+        }
 
         return view(Api::getView(), $data);
     }
