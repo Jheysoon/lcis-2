9c3d4b423561b92bf709bb66260fbfac84371d3d
Jayson Martinez <jlmartinez631@gmail.com>
2015-11-25 16:09:20 +0800

    create evaluation controller

---
+++ app/Http/Controllers/Dean/Evaluation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Dean;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Evaluation extends Controller
+{
+    //
+}
